454c787aec7f1a617eb8acb48e95076972b701b9
ValentinaZam <abc99-33@yandex.ru>
2023-09-29 20:14:17 +0300

    правка url

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://api.petprodject-hotelpanet.ru;";
+export const BASE_URL = "https://api.petprodject-hotelpanet.ru";
 //export const BASE_URL = "http://localhost:3000";
 
 const checkResponse = async (res) => {
