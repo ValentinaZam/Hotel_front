4573549525fa576b2795876cf98bf98ef8aea07d
ValentinaZam <abc99-33@yandex.ru>
2023-10-03 22:19:38 +0300

    4

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Navigate } from "react-router-dom"
 
 export const ProtectedRoute = ({ element: Component, ...props }) => {
-    console.log(props.loggedIn)
-    // alert("Необходима авторизация")
+
+
     return props.loggedIn ? <Component {...props} /> : <Navigate to="/" replace />
 }
